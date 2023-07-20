3c79b2573f017faeb6eea4b2d756a342449421af
z1004-2002 <zogningfranc@gmail.com>
2023-07-20 14:16:35 +0100

    end
    # Conflicts:
    #	src/main/java/com/vetrix/projetBD/gestionnaire/GestController.java
    #	src/main/java/com/vetrix/projetBD/gestionnaire/GestService.java

--- src/main/java/com/vetrix/projetBD/ProjetBdApplication.java
+++ src/main/java/com/vetrix/projetBD/ProjetBdApplication.java
@@ -1,15 +1,14 @@
 package com.vetrix.projetBD;
 
+import com.vetrix.projetBD.photo.PhotoStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({com.vetrix.projetBD.photo.PhotoStorageProperties.class})
+@EnableConfigurationProperties({PhotoStorageProperties.class})
 public class ProjetBdApplication {
-
 		public static void main(String[] args) {
 			SpringApplication.run(ProjetBdApplication.class, args);
 		}
-
 }
